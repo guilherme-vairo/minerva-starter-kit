58940cdb8481c9d331476cee868cf1c3b88e8951
Luiz dos Santos <luizsantos@localhost.localdomain>
2019-04-12 10:04:45 -0300

    teste de formulário netlify

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,18 +38,18 @@ export default ({ data }) => {
           </div>
         ))}
       </div>
+      <form name="contact" netlify>
+        <p>
+          <label>Name <input type="text" name="name" /></label>
+        </p>
+        <p>
+          <label>Email <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </Layout>
-    <form name="contact" netlify>
-      <p>
-        <label>Name <input type="text" name="name" /></label>
-      </p>
-      <p>
-        <label>Email <input type="email" name="email" /></label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
   )
 }
 
